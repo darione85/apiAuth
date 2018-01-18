7e0f633628488e54e9e90e3001bbc18c8a4cbdfb
dario <dario.rubado@cimafoundation.org>
2018-01-18 14:22:09 +0100

    testing jade

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var UserModel = require('./../models/authApi/userSchema')
+
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    winston.log({
+        lever:"info",
+        message:"INIT ROUTES"
+    });
   res.send('respond with a resource');
 });
 
 router.get('/login', function(req, res, next) {
-    res.send('respond with a resource');
+
+    // res.json({message:"user login"});
+    // res.json(viewsPath+"/login.html")
+    // res.sendFile(req.app.get('views')+"/login.html")
+    res.render('login', { title: 'Express' });
 });
 
 router.get('/logout', function(req, res, next) {
